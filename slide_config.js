50e25ba9f5e34199fe503687650e8a0ae0f448eb
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-03-27 21:15:52 +0800

    Add slides
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'ClojureScript Intro', 
+     subtitle: '', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/cljs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'coldnew', 
+     company: 'clojure.tw', 
+     gplus: '', 
+     twitter: 'coldnew', 
+     www: 'https://coldnew.github.io', 
+     github: 'http://github.com/coldnew', 
+   }]
+};
